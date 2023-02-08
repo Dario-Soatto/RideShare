ff34a83b293b5e353ba806f792c9c7c467c22d23
Dario Soatto <dsoatto23@students.polytechnic.org>
2023-02-08 08:40:52 -0800

    Cleaned up tester

--- Tester.java
+++ Tester.java
@@ -1,18 +1,22 @@
 public class Tester {
     public static void main(String [] args) {
         int total = 0;
+        int numStations = 32;
+        int numPassengers = 50;
+        int numCars = 20;
+        int numTrials = 100;
         for(int i = 0; i < 100; i++) {
-            Road r = new Road(31);
-            r.popPassengers(50);
-            r.popCars(20);
-            for(int j = 0; j < 32; j++) {
+            Road r = new Road(numStations);
+            r.popPassengers(numPassengers);
+            r.popCars(numCars);
+            for(int j = 0; j < numStations; j++) {
                 r.pickup();
                 r.move();
             }
             total += r.getRevenue();
-            System.out.println(r.getRevenue());
+            //System.out.println(r.getRevenue());
         }
-        System.out.println(total/100/20);
+        System.out.println(total/numTrials/numCars);
         
 
         //System.out.println(r);
